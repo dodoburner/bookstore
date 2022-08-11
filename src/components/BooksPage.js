f9e8df2210dae90b88a51b4ec810abcc29b693c6
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-11 15:04:29 +0200

    install uuid to generate unique key for books

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
+import Book from '../Book';
 
-const BooksPage = () => (
-  <div>
-    <Header />
-  </div>
-);
+const BooksPage = () => {
+  const books = [
+    {
+      author: 'Suzanne Collins',
+      title: 'The Hunger Games',
+      id: uuidv4(),
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <main>
+        {books.map((book) => (
+          <Book key={book.id} author={book.author} title={book.title} />
+        ))}
+      </main>
+    </div>
+  );
+};
 
 export default BooksPage;
