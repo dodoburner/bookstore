33c8bb59a4967093527d8fae37dfdf08b7950f83
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-08-11 12:54:04 +0200

    add jsx for header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <header>
+      <p className="logo">Bookstore CMS</p>
+      <ul>
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+      </ul>
+      <button></button>
+    </header>
+  )
+}
